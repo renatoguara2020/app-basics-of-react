c9d06eaeef1b14beae45156f10380ae37a820ddf
Renato Alves Soares <renatoguara2020@gmail.com>
2022-06-05 10:32:19 -0300

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 
 
 const App = () => {
+ 
+    const title = 'Blog Post';
+    const body = 'This is my Blog Post';
+    const comments = [
+
+        {id:1, text: 'Comment One'},
+        {id:2, text: 'Comment Two'},
+        {id:3, text: 'Comment Three'}
+    
+    ]
+
   return (
-    <Fragment>
-      <h1>Hello World no App com Components</h1>
-      <p>Este é um parágrafo</p>
-    </Fragment>
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+    
+
+      <div className="comments">
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+             {comments.map((comment, index)=>(
+                 
+
+                <li key={index}>{comment.text}</li>
+
+          ))}
+          </ul>
+      </div>
+    </div>
   )
 }
 
