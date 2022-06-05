6ae714aaa57b631f28c2d4168f6c5c19ca86ab58
Renato Alves Soares <renatoguara2020@gmail.com>
2022-06-05 16:05:52 -0300

    commit changes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import FeedbackData from './data/FeedbackData.js';
 const App = () => {
 
     const [feedback, setFeedback] = useState(FeedbackData);
+
+    const deleteFeedback = (id) => {
+        console.log("App", id)
+    }
   return (
 
    <Fragment> 
         <Header/>
         <div className="container">
-          <FeedbackList   feedback={feedback}/>
+          <FeedbackList   feedback={feedback} handleDelete={deleteFeedback}/>
         </div>
     </Fragment>    
   )
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
 import Card from './shared/Card';
+import {FaTimes} from 'react-icons/fa';
 
 
-export const FeedbackItem = ({item}) => {
+export const FeedbackItem = ({item, handleDelete}) => {
 
-    
-    
 
   return (
 
     <Card reverse={true}>
         <div className="num-display">{item.rating}</div>
+          <button className="close" onClick={()=> handleDelete(item.id)}>
+            <FaTimes color="purple"/>
+          </button>
         <div className="text-display">{item.text}</div>
     </Card>
 
