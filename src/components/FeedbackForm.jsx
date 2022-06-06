dda728209334a0726374cd06f6a7f09e412bdba7
Renato Alves Soares <renatoguara2020@gmail.com>
2022-06-06 11:37:11 -0300

    commit changes

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Card from './shared/Card';
 
 const FeeddbackForm = () => {
+
+  const [text, setText] = useState('');
+
+  const handleTextChange = (e) => {
+     setText(e.target.value)
+  }
   return (
     <Card>
         <form>
           <h2>How Would you rate your service with Us?</h2>
           {/*to-do  rating select Component*/}
-          
+          <div className="input-group">
+           <input onChange={handleTextChange} type="text" placeholder='Write a Review!' value={text} />
+           <button type="submit">Send</button>
+          </div>
+
         </form>
     </Card>
   )
