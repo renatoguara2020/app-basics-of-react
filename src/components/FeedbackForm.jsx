399d1bfddee7f1f429125d29338fcbf17d38efa9
Renato Alves Soares <renatoguara2020@gmail.com>
2022-06-06 11:14:04 -0300

    commit changes

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Card from './shared/Card';
 
 const FeeddbackForm = () => {
   return (
-    <div>
-        FeeddbackForm2022
-    </div>
+    <Card>
+        <form>
+          <h2>How Would you rate your service with Us?</h2>
+          {/*to-do  rating select Component*/}
+          
+        </form>
+    </Card>
   )
 }
 
